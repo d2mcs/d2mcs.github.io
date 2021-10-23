e332707c4d3d9b95e4711894ea30f8e723e589c2
d2mcs <porcupinathy@gmail.com>
2021-10-23 07:38:04 -0700

    refactored tiebreaker code to better accommodate future events

--- src/model/simulator.py
+++ src/model/simulator.py
@@ -2,7 +2,6 @@
 probabilities using Monte-Carlo sampling. Multiprocessing is used to
 speed up computation of results.
 """
-from itertools import combinations
 from multiprocessing import Pool
 import random
 import copy
@@ -12,6 +11,7 @@ from tqdm import tqdm
 
 from model.forecaster import PlayerModel, TeamModel
 from model.forecaster_glicko import Glicko2Model
+from model.tiebreakers import Tiebreaker
 
 class Simulator:
     """Generic simulator class to be subclassed by more specific
@@ -273,7 +273,7 @@ class Simulator:
                     output_f.write(f'  "{team}": {rating:.2f}\n')
             output_f.write("}\n")
 
-class EliminationBracket(Simulator):
+class TIEliminationBracket(Simulator):
     """16-team double elimination bracket Simulator. Like GroupStage,
     this class is currently designed specifically for the TI format
     (the seeding code assumes two groups of 8).
@@ -344,7 +344,7 @@ class EliminationBracket(Simulator):
 
         Returns
         -------
-        ranks : dict
+        dict
             dictionary mapping placement (in the form of a string) to
             teams which received that placement. For consistency all
             dict values are sets even for ranks that only can contain
@@ -397,265 +397,15 @@ class EliminationBracket(Simulator):
         ranks["1"].add(match[winner])
         return ranks
 
-class GroupStage(Simulator):
-    """Group Stage Simulator. Most of the code in this class is fairly
-    general but for now it is specifically written for a TI group stage
-    format.
+class TIGroupStage(Simulator):
+    """TI Group Stage Simulator: 9 team round-robin with additional
+    matches played for ties along 4th-5th and 8th-9th place, head-to-
+    head results used to break other ties.
 
     Parameters
     ----------
     Identical to Simulator. See above for details
     """
-
-    def _order_teams(self, teams, point_map):
-        """Given a list of teams and a mapping from team names to
-        points, creates an ordered list of teams where tied teams
-        occupy the same element of the list as a set.
-
-        Parameters
-        ----------
-        teams : list of str
-            list of team names
-        point_map : dict
-            Mapping between team names and point values
-
-        Returns
-        -------
-        list
-            Ordered list of teams. If two teams have the same number of
-            points, those teams are placed in a set at the same
-            position of the list. Example (where B and C are tied):
-
-                ["Team A", {"Team B", "Team C"}, "Team D"]
-        """
-        team_order = []
-        for team in sorted(teams, key=lambda t: -point_map[t]):
-            if len(team_order) != 0 and isinstance(team_order[-1], set):
-                if point_map[next(iter(team_order[-1]))] == point_map[team]:
-                    team_order[-1].add(team)
-                else:
-                    team_order.append(team)
-            elif (len(team_order) != 0
-                  and point_map[team_order[-1]] == point_map[team]):
-                team_order.append({team_order.pop(), team})
-            else:
-                team_order.append(team)
-        return team_order
-
-    def bo1_tiebreak(self, boundary, teams):
-        """Breaks a tie along a given boundary using round-robin bo1s.
-        Only ties along the boundary are considered, so in the case of
-        a multi-way tie (which is when this tiebreak method is used at
-        TI) even if teams aren't completely ordered tiebreaks will stop
-        once the teams along the boundary are ordered.
-
-        Parameters
-        ----------
-        boundary : tuple of (int, int)
-            Individual boundary to break a tie at. See documentation
-            for boundary_tiebreak for more information
-        teams : list of str
-            list of team names
-        """
-        tiebreak_points = {team: 0 for team in teams}
-        matches = list(combinations(teams, 2))
-
-        random.shuffle(matches)
-        for match in matches:
-            team1_win = self.sim_bo1(match[0], match[1])
-            tiebreak_points[match[1 - int(team1_win)]] += 1
-            tiebreak_points[match[int(team1_win)]] -= 1
-
-            # a 3-way tiebreaker may be cut off early if a team goes
-            # 2-0 or 0-2 in the first two matches and such a result
-            # resolves the tie without needing the third match
-            # (see TI8 for an example)
-            # in theory something similar could happen with a 4+ way
-            # tie but matches might be played simultaneously and there
-            # isn't any precedence to determine whether the matches
-            # would end early anyway so I explicitly only stop early
-            # for 3-way ties
-            if len(teams) == 3:
-                if (tiebreak_points[match[1-int(team1_win)]] == len(teams) - 1
-                      and boundary[0] == 0):
-                    break
-                if (tiebreak_points[match[int(team1_win)]] == 1 - len(teams)
-                      and boundary[1] == len(teams) - 1):
-                    break
-
-        team_order = self._order_teams(teams, tiebreak_points)
-
-        rank_start = 0
-        for i, team_sets in enumerate(team_order):
-            if not isinstance(team_sets, set):
-                rank_start += 1
-                continue
-            rank_end = rank_start + len(team_sets) - 1
-            if rank_start <= boundary[0] and rank_end >= boundary[1]:
-                break_pos = boundary[0] - rank_start
-                relative_boundary = (break_pos, break_pos + 1)
-                # if there's still a tie along the boundary,
-                # play more bo1s to resolve it
-                team_order[i:i+1] = self.bo1_tiebreak(
-                    relative_boundary, team_sets)
-            rank_start += len(team_sets)
-
-        return team_order
-
-    def boundary_tiebreak(self, tiebreak_boundaries, team_order, point_map):
-        """Breaks ties along a boundary by playing additional matches.
-        Once the boundary tie has been broken, no additional matches
-        are played. Following official TI rules, a bo3 is used for
-        2-way ties and bo1s are used for 3+ way ties.
-
-        Ties which aren't along a boundary which requires tiebreak
-        matches are not considered (these ties use head-to-head
-        results, which are covered by a different function)
-
-        Parameters
-        ----------
-        tiebreak_boundaries : List of tuple of int
-            List of positions (0-indexed) which require additional
-            matches in the case of a tie. For example, in the case of
-            TI (4th/5th and 8th/9th require tiebreakers) this would be:
-
-                [(3,4), (7,8)]
-
-            The second number is technically redundant but I find it
-            more intuitive/easier to remember
-        team_order : list
-            Team ordering as returned by _order_teams
-        point_map : dict
-            Mapping from team name to current points.
-
-        Returns
-        -------
-        team_order : list
-            Updated team ordering with boundary ties resolved
-        tiebreak_sizes : list of int
-            List containing the size of each broken tie
-        """
-        num_groups = len(team_order) # number of tied groups
-
-        rank_end = len(point_map) - 1
-        tiebreak_sizes = {boundary[0]: [] for boundary in tiebreak_boundaries}
-        for i, teams in enumerate(reversed(team_order)):
-            # no tie:
-            if not isinstance(teams, set):
-                rank_end -= 1
-                continue
-
-            # determine if tie is along a boundary for which
-            # additional matches are played
-            boundary_tie = False
-            rank_start = rank_end - (len(teams) - 1)
-            for boundary in tiebreak_boundaries:
-                if rank_start <= boundary[0] and rank_end >= boundary[1]:
-                    boundary_tie = True
-                    break
-            if not boundary_tie:
-                rank_end -= len(teams)
-                continue
-
-            # there is a tie, and it is along a boundary
-            if len(teams) == 2:
-                # 2-way tiebreak, play a single bo3
-                teams = list(teams)
-                result = self.sim_bo_n(3, teams[0], teams[1])
-                if result[0] == 2: # team 1 victory
-                    team_order[num_groups - i-1:num_groups - i] = [
-                        teams[0], teams[1]]
-                else:
-                    team_order[num_groups - i-1:num_groups - i] = [
-                        teams[1], teams[0]]
-                rank_end -= 2
-                tiebreak_sizes[boundary[0]].append(2)
-            else:
-                # multi-way tiebreak, play bo1s until tiebreak resolved
-                break_pos = boundary[0] - rank_start
-                relative_boundary = (break_pos, break_pos + 1)
-                reordered = self.bo1_tiebreak(relative_boundary, teams)
-                team_order[num_groups - i-1:num_groups - i] = reordered
-
-                rank_end -= len(teams)
-                tiebreak_sizes[boundary[0]].append(len(teams))
-
-        return team_order, tiebreak_sizes
-
-    def h2h_tiebreak(self, h2h_results, team_order, point_map):
-        """Breaks any remaining ties in an ordering using head-to-head
-        results. Following TI rules, if head-to-head results do not
-        resolve the tie results against lower seeded teams are
-        considered. If these do not resolve the tie, seeding is
-        assigned at random (TI rules specify a coin toss, but it is
-        technically possible to have a 3-way unresolved tie).
-
-        Parameters
-        ----------
-        h2h_results : dict
-            dict containing head-to-head results. Example:
-            {
-                "Team A": {"Team B": 2, "Team C": 1},
-                "Team B": {"Team A": 0, "Team C": 2},
-                "Team C": {"Team A": 1, "Team B": 0},
-            }
-            Note that the table should be complete, even though this
-            results in redundant information. The memory cost is not
-            significant and it makes the code a bit simpler.
-        team_order : list
-            Team ordering as returned by _order_teams
-        point_map : dict
-            Mapping from team name to current points.
-
-        Returns
-        -------
-        team_order : list
-            Updated team ordering with boundary ties resolved
-        """
-        # as long as the team order contains a set (unbroken tie),
-        # it will have fewer items than the total number of teams
-        while len(team_order) != len(point_map):
-            # ties must be broken bottom-up because if head-to-head results
-            # don't break ties results against lower-seeded teams do, so
-            # lower-seeded ties must be broken first
-            num_groups = len(team_order)
-            for i, teams in enumerate(reversed(team_order)):
-                if not isinstance(teams, set):
-                    continue
-                tiebreak_points = {team: 0 for team in teams}
-                # first, check head-to-head
-                for (team1, team2) in combinations(teams, 2):
-                    tiebreak_points[team1] += h2h_results[team1][team2]
-                    tiebreak_points[team2] += h2h_results[team2][team1]
-
-                tie_order = self._order_teams(teams, tiebreak_points)
-                if len(tie_order) != 1:
-                    team_order[num_groups - i - 1:num_groups - i] = tie_order
-                    break # any time a tie is broken, the process restarts
-
-                # head-to-head is even, so check results against lower seeds
-                next_seed = len(team_order) - i
-                tie_broken = False
-                while next_seed < len(team_order):
-                    team2 = team_order[next_seed]
-                    for team in teams:
-                        tiebreak_points[team] += h2h_results[team][team2]
-                    tie_order = self._order_teams(teams, tiebreak_points)
-                    if len(tie_order) != 1:
-                        tie_broken = True
-                        break
-                    next_seed += 1
-
-                if tie_broken:
-                    team_order[num_groups - i - 1:num_groups - i] = tie_order
-                    break # any time a tie is broken, the process restarts
-
-                # tie can't be broken, assign at random
-                tied_teams = list(teams)
-                random.shuffle(tied_teams)
-                team_order[num_groups - i - 1:num_groups - i] = tied_teams
-
-        return team_order
 
     def simulate(self, group, matches):
         """Simulates a single group stage and returns the resulting
@@ -679,9 +429,12 @@ class GroupStage(Simulator):
 
         Returns
         -------
-        points : list of list(str, int)
+        list of list(str, int)
             Sorted list of teams and points. Example:
                 [["Team A", 10], ["Team B", 9], ["Team C", 5]]
+        dict
+            dict mapping each boundary to the number of teams tied
+            along that boundary
         """
         records = {team: [0,0,0] for team in group}
         h2h_results = {team: {} for team in group}
@@ -714,12 +467,14 @@ class GroupStage(Simulator):
         point_map = {}
         for team, record in records.items():
             point_map[team] = record[0]*2 + record[1]
-        team_order = self._order_teams(point_map.keys(), point_map)
 
-        team_order, tiebreak_sizes = self.boundary_tiebreak([(3,4), (7,8)],
-            team_order, point_map)
-        team_order = self.h2h_tiebreak(h2h_results,
-            team_order, point_map)
+        tiebreaker = Tiebreaker(self)
+        team_order = tiebreaker.order_teams(point_map.keys(), point_map)
+
+        team_order, tiebreak_sizes = tiebreaker.boundary_tiebreak(
+            [(3,4), (7,8)], team_order, point_map)
+        team_order = tiebreaker.h2h_tiebreak(h2h_results,
+                                             team_order, point_map)
         points = [(team, point_map[team]) for team in team_order]
 
         return points, tiebreak_sizes
@@ -748,11 +503,12 @@ class TISimulator(Simulator):
         Returns
         -------
             dict
-                Ordered (team, points) tuple for each group
+                Ordered (team, points) tuple for each group.
             dict
-                Size of tiebreak required along each boundary
+                Size of tiebreak required along each boundary. 0 if no
+                tiebreaker matches were necessary.
             dict
-                Mapping from ranks to the teams placed at that rank
+                Mapping from ranks to the teams placed at that rank.
         """
         if not self.static_ratings:
             # if ratings are updated during a simulation, the code
@@ -761,14 +517,14 @@ class TISimulator(Simulator):
             working_model = copy.deepcopy(self.model)
         else:
             working_model = self.model
-        gs_sim = GroupStage(working_model, "team",
+        gs_sim = TIGroupStage(working_model, "team",
                             self.rosters, self.static_ratings)
 
         points_a, tiebreak_sizes_a = gs_sim.simulate(groups["a"], matches["a"])
         points_b, tiebreak_sizes_b = gs_sim.simulate(groups["b"], matches["b"])
 
-        bracket_sim = EliminationBracket(gs_sim.model, "team",
-                                         self.rosters, self.static_ratings)
+        bracket_sim = TIEliminationBracket(gs_sim.model, "team",
+                                           self.rosters, self.static_ratings)
         bracket_sim.seed({"a": [p[0] for p in points_a],
                           "b": [p[0] for p in points_b]})
         ranks = bracket_sim.simulate()
@@ -801,8 +557,8 @@ class TISimulator(Simulator):
         else:
             working_model = self.model
 
-        bracket_sim = EliminationBracket(working_model, "team",
-                                         self.rosters, self.static_ratings)
+        bracket_sim = TIEliminationBracket(working_model, "team",
+                                           self.rosters, self.static_ratings)
         bracket_sim.load_bracket(bracket)
         ranks = bracket_sim.simulate()
 
@@ -886,10 +642,9 @@ class TISimulator(Simulator):
                                 final_rank_probs[team]["17-18"] += 1/n_trials
                             point_rank_probs[group][team][record][i] += 1
                             record_probs[group][team][record] += 1/n_trials
-                        for boundary, sizes in tiebreak_sizes[group].items():
-                            for size in sizes:
-                                tiebreak_probs[group][boundary][
-                                    size - 2] += 1/n_trials
+                        for boundary, size in tiebreak_sizes[group].items():
+                            tiebreak_probs[group][boundary][
+                                size - 2] += 1/n_trials
                     for rank, teams in ranks.items():
                         for team in teams:
                             final_rank_probs[team][rank] += 1/n_trials
@@ -947,9 +702,8 @@ class TISimulator(Simulator):
                     final_rank_probs[team]["17-18"] += 1
                 point_rank_probs[group][team][record][i] += 1
                 record_probs[group][team][record] += 1
-            for boundary, sizes in tiebreak_sizes[group].items():
-                for size in sizes:
-                    tiebreak_probs[group][boundary][size - 2] += 1
+            for boundary, size in tiebreak_sizes[group].items():
+                tiebreak_probs[group][boundary][size - 2] += 1
 
         # get_sample doesn't update team ratings, so that has to
         # happen seperately. sim_group_stage and sim_main_event aren't
--- src/tests/test_tiebreakers.py
+++ src/tests/test_tiebreakers.py
@@ -1,20 +1,22 @@
 import pytest
 from pathlib import Path
 
-from model.simulator import GroupStage
+from model.simulator import Simulator
+from model.tiebreakers import Tiebreaker
 
 @pytest.fixture(scope='session')
-def sim():
-    sim = GroupStage.from_ratings_file(
+def tiebreaker():
+    sim = Simulator.from_ratings_file(
         "tests/test_ratings.json", k=55, static_ratings=True)
-    return sim
+    tiebreaker = Tiebreaker(sim)
+    return tiebreaker
 
 class TestH2HTiebreak:
     """Head-to-head tiebreaks. Each test is run several times because
     the tiebreak will revert to a coinflip if it fails, so the tests
     should pass consistently
     """
-    def test_basic_h2h(self, sim):
+    def test_basic_h2h(self, tiebreaker):
         for _ in range(10):
             h2h = {
                 "Team A": {"Team B": 0, "Team C": 2, "Team D": 1},
@@ -24,10 +26,10 @@ class TestH2HTiebreak:
             }
             team_order = ["Team D", {"Team A", "Team B"}, "Team C"]
             point_map = {"Team A": 3, "Team B": 3, "Team C": 1, "Team D": 5}
-            reordered = sim.h2h_tiebreak(h2h, team_order, point_map)
+            reordered = tiebreaker.h2h_tiebreak(h2h, team_order, point_map)
             assert reordered == ["Team D", "Team B", "Team A", "Team C"]
 
-    def test_3x3_h2h(self, sim):
+    def test_3x3_h2h(self, tiebreaker):
         for _ in range(10):
             h2h = {
                 "Team A": {"Team B": 2, "Team C": 2, "Team D": 0, "Team E": 0},
@@ -39,10 +41,10 @@ class TestH2HTiebreak:
             team_order = ["Team E", {"Team C", "Team A", "Team B"}, "Team D"]
             point_map = {"Team A": 4, "Team B": 4, "Team C": 4,
                          "Team D": 2, "Team E": 6}
-            reordered = sim.h2h_tiebreak(h2h, team_order, point_map)
+            reordered = tiebreaker.h2h_tiebreak(h2h, team_order, point_map)
             assert reordered == ["Team E","Team A","Team B","Team C","Team D"]
 
-    def test_lower_seed_h2h(self, sim):
+    def test_lower_seed_h2h(self, tiebreaker):
         for _ in range(10):
             h2h = {
                 "Team A": {"Team B": 1, "Team C": 2, "Team D": 1},
@@ -52,12 +54,12 @@ class TestH2HTiebreak:
             }
             team_order = [{"Team B", "Team A"}, "Team D", "Team C"]
             point_map = {"Team A": 4, "Team B": 4, "Team C": 1, "Team D": 3}
-            reordered = sim.h2h_tiebreak(h2h, team_order, point_map)
+            reordered = tiebreaker.h2h_tiebreak(h2h, team_order, point_map)
             # Team B should be higher seed due to having a
             # better result against team D
             assert reordered == ["Team B", "Team A", "Team D", "Team C"]
 
-    def test_random_h2h(self, sim):
+    def test_random_h2h(self, tiebreaker):
         ranks = {
             "Team A": [0, 0, 0], "Team B": [0, 0, 0], "Team C": [0, 0, 0]
         }
@@ -69,7 +71,7 @@ class TestH2HTiebreak:
             }
             team_order = [{"Team B", "Team A", "Team C"}]
             point_map = {"Team A": 3, "Team B": 3, "Team C": 3}
-            reordered = sim.h2h_tiebreak(h2h, team_order, point_map)
+            reordered = tiebreaker.h2h_tiebreak(h2h, team_order, point_map)
             # Teams are equal, so tiebreak should be random
             for i in range(3):
                 ranks[reordered[i]][i] += 1/100
@@ -78,47 +80,47 @@ class TestH2HTiebreak:
                 assert prob >= .1 and prob <= .99
 
 class TestBoundaryTiebreak:
-    def test_no_tiebreak_needed(self, sim):
+    def test_no_tiebreak_needed(self, tiebreaker):
         team_order = ["Team A", "Team B", {"Team C", "Team D"}]
         point_map = {"Team A": 3, "Team B": 2, "Team C": 1, "Team D": 1}
 
         # tie isn't along the boundary, so nothing should change
-        reordered, _ = sim.boundary_tiebreak([(0, 1)], team_order, point_map)
+        reordered, _ = tiebreaker.boundary_tiebreak([(0, 1)], team_order, point_map)
         assert reordered == team_order
-        reordered, _ = sim.boundary_tiebreak([(1, 2)], team_order, point_map)
+        reordered, _ = tiebreaker.boundary_tiebreak([(1, 2)], team_order, point_map)
         assert reordered == team_order
 
-    def test_2x2_tiebreak(self, sim):
+    def test_2x2_tiebreak(self, tiebreaker):
         probs = {"Team C": 0, "Team D": 0}
         for _ in range(100):
             team_order = ["Team A", "Team B", {"Team C", "Team D"}]
             point_map = {"Team A": 3, "Team B": 2, "Team C": 1, "Team D": 1}
-            reordered,_ = sim.boundary_tiebreak([(2,3)], team_order, point_map)
+            reordered,_ = tiebreaker.boundary_tiebreak([(2,3)], team_order, point_map)
             assert isinstance(reordered[2], str) # ensure tie was broken
             probs[reordered[2]] += 1/100
         # both teams should win ~50 times
         for team, prob in probs.items():
             assert prob >= .1 and prob <= .99
 
-    def test_3x3_tiebreak(self, sim):
+    def test_3x3_tiebreak(self, tiebreaker):
         probs = {"Team B": 0, "Team C": 0, "Team D": 0}
         for _ in range(100):
             team_order = ["Team A", {"Team B", "Team C", "Team D"}]
             point_map = {"Team A": 3, "Team B": 1, "Team C": 1, "Team D": 1}
-            reordered,_ = sim.boundary_tiebreak([(2,3)], team_order, point_map)
+            reordered,_ = tiebreaker.boundary_tiebreak([(2,3)], team_order, point_map)
             assert isinstance(reordered[-1], str)
             probs[reordered[-1]] += 1/100
 
         for team, prob in probs.items():
             assert prob >= .1 and prob <= .99
 
-    def test_4x4_tiebreak(self, sim):
+    def test_4x4_tiebreak(self, tiebreaker):
         probs = {"Team A": 0, "Team B": 0, "Team C": 0, "Team D": 0}
         lengths = [0, 0, 0]
         for _ in range(1000):
             team_order = [{"Team A", "Team B", "Team C", "Team D"}]
             point_map = {"Team A": 1, "Team B": 1, "Team C": 1, "Team D": 1}
-            reordered,_ = sim.boundary_tiebreak([(2,3)], team_order, point_map)
+            reordered,_ = tiebreaker.boundary_tiebreak([(2,3)], team_order, point_map)
             assert isinstance(reordered[-1], str) # ensure tie was broken
             lengths[len(reordered) - 3] += 1/1000
             probs[reordered[-1]] += 1/1000
@@ -129,13 +131,13 @@ class TestBoundaryTiebreak:
         for length_prob in lengths:
             assert length_prob >= .01 and length_prob <= .99
 
-    def test_multitie(self, sim):
+    def test_multitie(self, tiebreaker):
         upper_probs = {"Team A": 0, "Team B": 0}
         lower_probs = {"Team C": 0, "Team D": 0}
         for _ in range(100):
             team_order = [{"Team A", "Team B"}, {"Team C", "Team D"}]
             point_map = {"Team A": 2, "Team B": 2, "Team C": 1, "Team D": 1}
-            reordered,_ = sim.boundary_tiebreak([(0,1), (2,3)],
+            reordered,_ = tiebreaker.boundary_tiebreak([(0,1), (2,3)],
                 team_order, point_map)
             assert len(reordered) == 4 # ensure ties were broken
 
